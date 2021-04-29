3e009a423e721710223f5af9e5bd7fdcf6f51b3d
darshan-zare <drshnzr.98@gmail.com>
2021-04-30 00:10:19 +0530

    Assignments added2

--- Mainclass.java
+++ Mainclass.java
@@ -100,22 +100,7 @@ public class Mainclass
            			for (int i = 0; i < arr.length; i++)   
            			{       
            				System.out.println(arr[i]);   
-           			}  
-        	  /* 		for (int i=0; i<arr.length; i++)   
-        	   		{  
-        	   			for (int j=i+1; j<arr.length; j++)   
-        	   			{  
-        	   				int tmp = 0;  
-        	   				if (arr[i]>arr[j])   
-        	   				{  
-        	   					tmp=arr[i];  
-        	   					arr[i]=arr[j];  
-        	   					arr[j]=tmp;  
-        	   				}  
-        	   			}  
-  
-        	   			System.out.println(arr[i]);  
-        	   		}  */
+           			}
         	   		break;
            
            default : System.exit(0);
